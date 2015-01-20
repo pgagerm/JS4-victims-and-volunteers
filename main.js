e22e1b3591c5929f6e0d03fe635122c6a5872c26
Michael Germany <mike@rickgermany.com>
2015-01-20 12:29:49 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//JS4 Victims and Volunteers
+
+var victims = prompt("How many disaster victims do you wish to enter?");
+
+for (i = 0; i < victims; i++) {
+
+	var victimName = prompt("Please enter your name.");
+
+	var victimPhone = prompt("Please enter your phone number.");
+
+	var victimStreet = prompt("Please enter your street.");
+}
+
+var victimsList = [];
+victimsList.push(victimName, victimPhone, victimStreet);
+
+var volunteers = prompt("How many volunteers do you have?");
+
+for (i = 0; i < volunteers; i++) {
+
+	var volunteersName = prompt("Please enter your name.");
+
+	var volunteersPhone = prompt("Please enter your phone number.");
+
+	var volunteersStreet = prompt("Please enter your street.");
+}
+
+var volunteersList = [];
+volunteersList.push(volunteersName, volunteersPhone, volunteersStreet);
+
+var victimsLog = "Victims: " + victims + "\n" +
+				"Volunteers: " + volunteers + "\n";
+
+victimsLog += "Victims:\n";
+
+for (i = 0; i < victims; i++) {
+	victimsLog += " Name: "+ victimName + ", " +
+				"Phone: " + victimPhone + ", " +
+				"Street: " + victimStreet + "\n";
+			}
+
+victimsLog += "Volunteers:\n";
+
+for (i = 0; i < volunteers; i++) {
+	victimsLog += " Name: "+ volunteersName + ", " +
+				"Phone: " + volunteersPhone + ", " +
+				"Street: " + volunteersStreet + "\n";
+			}	
+
+alert(victimsLog);
